a84391f9f21f9f3c6d8bf6077e7d037d8e68c12d
vsramanujan <rama.psbb@gmail.com>
2021-01-18 12:00:28 +0530

    fix: Flash of unstyled font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './styles/custom.css'],
   darkMode: 'class',
@@ -9,6 +11,9 @@ module.exports = {
       },
       boxShadow: {
         white: '0 4px 14px 0 rgba(255, 255, 255, 0.39)',
+      },
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans]
       }
     },
   },
